42da94d6749f8f9730c7e45eed0f0a929c9d8fce
root <root@gitmusicdev.(none)>
2014-04-23 03:09:48 +0000

    initial commit

---
+++ app/views/homepage/loggedin/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.loggedin.master')
+
+@section('content')
+Content goes here ya bish. I couldn't put icons for the menu items cause I didn't have the icons from da design but iss all good, we do em later. This is the layout and feel for what it will look like when a user is logged in. Other pages will show up when the user is not logged in. I will finalize those pages in the coming week.<br />
+- CSS Ramin
+@stop
