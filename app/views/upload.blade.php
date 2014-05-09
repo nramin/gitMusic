fef1b8540f1b8eb461b1d9ea695cc3bac5468f5d
Ramin Nehchiri <nramin@uw.edu>
2014-05-09 20:39:55 +0000

    Created the HTML/CSS for the upload dialog

--- app/views/upload.blade.php
+++ app/views/upload.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.loggedin.master')
 
 @section('content')
-	<h2> Yo you could upload here</h2>
+	<div id="upload-dialog">
+		<form method="post" action="{{ route('upload') }}">
+		    <div id="upload-dialog-title">Upload</div>
+		    <input type="text" placeholder="Song Name" id="songname">
+		    <div id="file-container">
+		        <input type="file" name="song" accept="*" id="file">
+		    </div>
+		    <input type="submit" value="Upload" id="submit">
+		</form>
+	</div>
 @stop
