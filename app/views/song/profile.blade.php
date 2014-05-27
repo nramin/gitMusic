169018dd395802b590ab373ed08d8db36054daa0
ewadav <ewadav@uw.edu>
2014-05-27 15:10:59 -0700

    small song profile changes

--- app/views/song/profile.blade.php
+++ app/views/song/profile.blade.php
@@ -4,10 +4,14 @@
 	<h1>Songname: {{ $song->songname }}</h1>
 	<p>Artist: {{ HTML::linkRoute('userProfile', $song->user->username , array($song->user->username)) }}</p>
 	<p>Likes: {{ $song->likes() }}</p>
-	@if (Auth::user()->likes_song($song->getId()))
-		<h4>You like it also</h4>
+	@if (Auth::user())
+		@if (Auth::user()->likes_song($song->getId()))
+			<h4>You like it also</h4>
+		@else
+			<button>Like it!</button>
+		@endif
 	@else 
-		<button>Like it!</button>
+		<button>Signup to Like it!</button>
 	@endif
 	
 	{{ HTML::style('assets/css/jplayer/not.the.skin.css') }}
@@ -25,7 +29,7 @@
 
 			var myOtherOne = new CirclePlayer("#jquery_jplayer_2",
 			{
-				m4a:"https://s3-us-west-2.amazonaws.com/gitmusic/Step+(Featuring+Danny+Brown%2C+Heems+%26+Despot).mp3"
+				m4a: "<?php echo($song->url); ?>"
 			}, {
 				cssSelectorAncestor: "#cp_container_2"
 			});
@@ -34,7 +38,7 @@
 	
 	<div id="jquery_jplayer_2" class="cp-jplayer"></div>
 
-		<div class="prototype-wrapper"> <!-- A wrapper to emulate use in a webpage and center align -->
+		<div class="music_player"> <!-- A wrapper to emulate use in a webpage and center align -->
 
 			<div id="cp_container_2" class="cp-container">
 				<div class="cp-buffer-holder">
@@ -54,12 +58,16 @@
 		</div>
 	</div>
 
-	<h2>Comments:</h2>
-	<ul>
-		@foreach ($song->comments as $comment)
-			<li>{{ HTML::linkRoute('userProfile', $comment->user->username , array($comment->user->username)) }}</li>
-			<li>{{ $comment->content }}</li>
-		@endforeach
-	</ul>
+	@if (count($song->comments) > 0)
+		<h2>Comments:</h2>
+		<ul>
+			@foreach ($song->comments as $comment)
+				<li>{{ HTML::linkRoute('userProfile', $comment->user->username , array($comment->user->username)) }}</li>
+				<li>{{ $comment->content }}</li>
+			@endforeach
+		</ul>
+	@else
+		<h3>No comments :(</h3>
+	@endif
 
 @stop
