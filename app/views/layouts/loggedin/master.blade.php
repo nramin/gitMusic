cc0cc756f1dd9b70f5889ee8f9bf1561bb2af607
Ramin Nehchiri <nramin@uw.edu>
2014-05-29 05:31:17 +0000

    Updated master page

--- app/views/layouts/loggedin/master.blade.php
+++ app/views/layouts/loggedin/master.blade.php
@@ -4,11 +4,26 @@
         <meta charset="utf-8">
         <title>Git Music</title>
         <meta name="description" content="Music Collabaration, and Discovery">
-        <meta name="author" content="">
+        <meta name="author" content="GitMusic">
         
         {{ HTML::style('assets/css/loggedin/main.css') }}
         {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js') }}
         {{ HTML::script('assets/js/main.js') }}
+
+        {{ HTML::style('assets/css/jplayer/360player.css') }}
+        {{ HTML::style('assets/css/jplayer/flashblock.css') }}
+        {{ HTML::style('assets/css/jplayer/360player-visualization.css') }}
+
+        {{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js') }}
+        {{ HTML::script('assets/js/soundmanager2.js') }}
+        {{ HTML::script('assets/js/berniecode-animator.js') }}
+        {{ HTML::script('assets/js/360player.js') }}
+        {{ HTML::script('assets/js/bootstrap-player.js') }}
+
+        <script type="text/javascript" src="//use.typekit.net/rqj5dqa.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+        <script type="text/javascript" src="//use.typekit.net/jhd1olv.js"></script>
+        <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
         
     </head>
 
@@ -20,9 +35,9 @@
         @section('topnav')
         <nav>
             <ul>
-                <li><a href="{{ route('upload') }}">Upload</a></li>
-                <li><a href="{{ route('settings') }}">Settings</a></li>
-                <li><a href="{{ route('logout-get') }}">Logout</a></li>
+                <li><a href="{{ route('upload') }}" class="upload">Upload</a></li>
+                <li><a href="{{ route('settings') }}" class="settings">Settings</a></li>
+                <li><a href="{{ route('logout-get') }}" class="logout">Logout</a></li>
             </ul>
         </nav>
         @show
@@ -30,8 +45,21 @@
     </section>
 
     <section id="sidebar">
-
-        @yield('sidebar', '')
+        <?php $current_user = Auth::user(); ?>
+        @if ($current_user)
+            @if (sizeof($current_user->songs) < 1)
+                <p>You have no projects</p>
+            @else
+                <div class="sidebar-menu-title">Your<br />Projects:</div>
+                <div class="sidebar-menu-line"></div>
+                    @foreach ($current_user->songs as $song)
+                    <div class="sidebar-menu-item">
+                        {{ HTML::linkRoute('songProfile', '', array($current_user, $song->songname), array('class' => 'sidebar-menu-circle')) }}
+                        {{ HTML::linkRoute('songProfile', $song, array($current_user, $song->songname), array('class' => 'sidebar-menu-text')) }}
+                    </div>
+                    @endforeach
+            @endif
+        @endif
 
     </section>
 
@@ -40,6 +68,14 @@
         @yield('content')
 
     </section>
+<!-- 
+    <footer id="footer">
+       
+        @section('footer')
+            <h4>This is where we have ourselves a footer</h4>
+        @show
+
+    </footer> -->
 
     </body>
 </html>
