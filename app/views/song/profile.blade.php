125d0d20da2112aec0dabd2164538e5cb17ad4fe
ewadav <ewadav@uw.edu>
2014-05-29 20:47:28 -0700

    fixed the merge bullshit

--- app/views/song/profile.blade.php
+++ app/views/song/profile.blade.php
@@ -8,41 +8,18 @@
 
 	<h1 id='songHeader'>Politics of Little<br>League Baseball</h1>
 	<div class='version'>
-      <div class='player'></div>
-      <p class='songInfoCollab'>
-        Version 5<br>
-        May 20, 2014<br>
-        Uploaded by Duba<br>
-        <span class='downloadCollab'>Download</span>
-      </p>
+        <div class='player'></div>
+	        <p class='songInfoCollab'>
+	            Version 5<br>
+	            May 20, 2014<br>
+	            Uploaded by Duba<br>
+	        <span class='downloadCollab'>Download</span>
+	        </p>
+	    </div>
     </div>
 
     <div class='break'>
       <div class='line'>
-    </div>
-
-	<div id="songpage-details">
-		<span>Version: (we need to work on getting this)</span>
-		<span>{{ $song->updated_at }}</span>
-		<span>Uploaded by {{ HTML::linkRoute('userProfile', $song->user->username , array($song->user->username)) }}</span>
-		<span>Likes: <span class="song-likes">{{ $song->likes() }}</span></span>
-		
-		@if (Auth::user())
-			@if (Auth::user()->likes_song($song->getId()))
-				<h4>You like it also</h4>
-			@else
-				<span><button class="like-song" data-song-id="<?php echo $song->getId(); ?>">Like</button></span>
-
-    <div class='version'>
-	  <div class='player'></div>
-	      <p class='songInfoCollab'>
-	        Version 4<br>
-	        May 13, 2014<br>
-	        Uploaded by Duba<br>
-	        <span class='downloadCollab'>Download</span>
-	      </p>
-      </div>
-  
     </div>
 
 	<div id='raminSucks'>
@@ -54,34 +31,26 @@
 
 		<div id="songpage-player">
 	     	<div class="ui360 ui360-vis"><a href="<?php echo $song->url ?>"></a></div>
-		</div>
-
-		 <h1 id='songHeader'>Politics of Little<br>League Baseball</h1>
-
-	            
+	    </div>
 
 		<div id="songpage-details">
 			<span>Version: (we need to work on getting this)</span>
 			<span>{{ $song->updated_at }}</span>
 			<span>Uploaded by {{ HTML::linkRoute('userProfile', $song->user->username , array($song->user->username)) }}</span>
-			<span>Likes: {{ $song->likes() }}</span>
+			<span>Likes: <span class="song-likes">{{ $song->likes() }}</span></span>
 			
 			@if (Auth::user())
 				@if (Auth::user()->likes_song($song->getId()))
 					<h4>You like it also</h4>
 				@else
-					<span><a href="#">Like</a></span>
+					<span><button class="like-song" data-song-id="<?php echo $song->getId(); ?>">Like</button></span>
 				@endif
 			@else 
 				<span><a href="#">Sign up to like</a></span>
 			@endif
 
-		@else 
-			<span><a href="#">Sign up to like</a></span>
-		@endif
-		<!-- <span><a href="#" >Download</a></span> -->
-
 		{{ HTML::link($song->zip_url, 'Download Project'); }}
+		</div>
 	</div>
 
 	<div id="songpage-versions">
@@ -97,25 +66,24 @@
 		@endif
 	</div>
 
-		<div id="songpage-comments">
-			@if (count($song->comments) > 0)
-				<h2>Comments:</h2>
-					@foreach ($song->comments as $comment)
-						<div class="songpage-comment">
-							{{ HTML::linkRoute('userProfile', $comment->user->username , array($comment->user->username)) }}:
-							{{ $comment->content }}
-						</div>
-					@endforeach
-			@else
-				<h3>No comments :(</h3>
-			@endif
-		</div>
+	<div id="songpage-comments">
+		@if (count($song->comments) > 0)
+			<h2>Comments:</h2>
+				@foreach ($song->comments as $comment)
+					<div class="songpage-comment">
+						{{ HTML::linkRoute('userProfile', $comment->user->username , array($comment->user->username)) }}:
+						{{ $comment->content }}
+					</div>
+				@endforeach
+		@else
+			<h3>No comments :(</h3>
+		@endif
 	</div>
-
 
 	<div id='donutHolder'>
 	    <svg class="donut"></svg>
 	    	<p id='donutData'>fuck off</p>
 	    <svg class="path"></svg>
-	  </div>
+	</div>
+
 @stop
