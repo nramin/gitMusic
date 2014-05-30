f677855b98217c5f1cca7571c7d3597cf851be79
Ramin Nehchiri <nramin@uw.edu>
2014-05-30 02:47:25 +0000

    asdf

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-	var page_height = $(window).height();
-	var topbar_height = $('#topbar').height();
-	$('#sidebar').css('height', page_height - topbar_height);
+	sidebar_height();
+	$( window ).resize(function() {
+		sidebar_height();
+	}
+	function sidebar_height() {
+		var page_height = $(window).height();
+		var topbar_height = $('#topbar').height();
+		$('#sidebar').css('height', page_height - topbar_height);
+	}
 });
