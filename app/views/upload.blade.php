c9844cec0aca751b341b4eea23a8c68a71a193ca
Michael Beswetherick <mbeswetherick@gmail.com>
2014-05-30 12:51:59 -0700

    css god

--- app/views/upload.blade.php
+++ app/views/upload.blade.php
@@ -4,8 +4,8 @@
 	<div id="upload-dialog">
 		<form method="post" action="{{ route('upload') }}" enctype="multipart/form-data">
 		    <div id="upload-dialog-title">Upload</div>
-		    <input type="text" name="songname" placeholder="Song Name" id="songname">
-		    <div id="file-container">
+		    <input type="text" class='uploadField' name="songname" placeholder="Song Name" id="songname">
+		    <div id="file-container" class='uploadField'>
 		        <input type="file" name="songfile" accept="audio/*" id="file">
 		        <input type="file" name="projectfile" accept="application/zip">
 		    </div>
