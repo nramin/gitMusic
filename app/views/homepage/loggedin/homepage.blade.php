2895c52bce90d5772c2c80806392b98f02db6644
Vijai <vjr123@gmail.com>
2014-05-31 11:38:51 -0700

    up to date

--- app/views/homepage/loggedin/homepage.blade.php
+++ app/views/homepage/loggedin/homepage.blade.php
@@ -22,9 +22,9 @@
             	<h2 class='songHeader'>{{ HTML::linkRoute('songProfile', $song, array($song->user, $song->songname)) }}</h2>
             	<p class='songByline'>By<br>Lady the Beard</p>
             </div>
-            <div class='box'>
+            <div class='box' style="background-image: url('/assets/img/jfk.jpg'); background-repeat: no-repeat;">
               <div class='songInfo'>
-                <img class='download' src='download1.png' alt='chill' />
+                <img class='download' src='download.png' alt='chill' />
               </div>
             </div>
           </div>
