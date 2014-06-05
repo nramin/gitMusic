04d43a4dd9d1669d51e3165a717a541ec17b09e3
Vijai Rajpal <vjr123@gmail.com>
2014-06-05 03:21:07 -0700

    upload for versions

--- app/controllers/VersionController.php
+++ app/controllers/VersionController.php
@@ -4,28 +4,41 @@ class VersionController extends BaseController {
 
 	public function versionUpload()
 	{
-        $file = Input::all();
-        $zip = $file['projectfile'];
-        $mp3 = $file['songfile'];
-        $parent_song_id = $file['parentfile'];
-        $user = Auth::user();
-        $song_id = $file['song_id'];
-        $song = Song::find($song_id);
-        $dest = '/var/www/gitmusic/uploads';
-        $existing_versions = Version::where('song_id', '=', $song_id)->count();
-        $version_number = $existing_versions + 1;
-        $version_string = strval($version_number);
-        $pretty_songname = $song->pretty_songname;
-        $filename = $pretty_songname . '_' . $version_string . '.zip';
-        $zipname = $pretty_songname . '_' . $version_string . '.zip';
-        $picname = $pretty_songname . '_' . $version_string . '.zip';
+        
+        if(Input::hasFile('songfile'))
+        {
+            $file = Input::all();
+            $zip = $file['projectfile'];
+            $mp3 = $file['songfile'];
+            $user = Auth::user();
+            $song_id = $file['song_id'];
+            $song = Song::find($song_id);
+            $dest = '/var/www/gitmusic/uploads';
+            $existing_versions = Version::where('song_id', '=', $song_id)->count();
+            $version_number = $existing_versions + 1;
+            $version_string = strval($version_number);
+            $pretty_songname = $song->pretty_songname;
+            $filename = $pretty_songname . '_' . $version_string . '.mp3';
+            $zipname = $pretty_songname . '_' . $version_string . '.zip';
+            $file->move($dest, $filename);
+            $zip->move($dest, $zipname);
+            $destination_filepath_file = $dest . '/' . $filename;
+            $destination_filepath_zip = $dest . '/' . $zipname;
 
-        $create_versions = Version::create(array(
-                    'user_id' => $user_id,
-                    'song_id' => $song_id,
-                    'version_number' => $version_number,
-                    'url' => 
-                ));
+            $create_versions = Version::create(array(
+                'user_id' => $user_id,
+                'song_id' => $song_id,
+                'version_number' => $version_number,
+            ));
+
+            $create_versions->sendToS3($destination_filepath_file, $user, $filename);
+            $create_versions->sendToS3($destination_filepath_zip, $user, $zipname);
+
+            return Redirect::route('songProfile', array($user->pretty_username, $songname_nospaces));
+        } else {
+            echo 'oops';
+        }
+        
 
 	}
 }
--- app/models/Version.php
+++ app/models/Version.php
@@ -21,4 +21,47 @@ class Version extends Eloquent {
         return $query->orderBy('created_at', 'desc');
     }
 
+    public function sendToS3($destination_filepath, $user, $filename) {
+        $user = $user . "\/versions/";
+        $s3 = AWS::get('s3');
+        $result = $s3 ->putObject(array(
+            'ACL'        => 'public-read',
+            'Bucket'     => 'gitmusic',
+            'Key'        => $user . $filename,
+            'SourceFile' => $destination_filepath    
+        ));
+        if(isset($result))
+        {
+            File::delete($destination_filepath);
+            if($this->setURL($result['ObjectURL'])) {
+                return true;
+            } else {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+    public function setURL($url)
+    {
+        if(isset($url) and $version = Version::find($this->getId()))
+        {
+            $version = Version::find($this->getId());
+            if(strpos($url, '.mp3') == true){
+                $user->url = $url;
+                $user->save();
+                return true;
+            } else {
+                $user->zip_url = $url;
+                $user->save();
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+
 }
