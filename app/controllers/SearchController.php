c6d8ab1f7b525c969e133e1a11980c2d1a904d33
ewadav <ewadav@uw.edu>
2014-06-05 02:50:39 -0700

    fixed search sql special characters

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -16,17 +16,23 @@ class SearchController extends BaseController {
 		$username_to_user = array();
 		$songname_to_song = array();
 		foreach ($songs as $song) {
-			$song_score = DB::select(DB::raw("SELECT jaro_winkler_similarity('" . $song->songname . "','" . $term . "') AS score"));
+			$clean_songname = preg_replace("![^a-z0-9]+!i", "", $song->songname);
+			$song_score = DB::select(DB::raw("SELECT jaro_winkler_similarity('" . $clean_songname . "','" . $term . "') AS score"));
 			$rankings[$song->songname] = $song_score;
 			$songname_to_song[$song->songname] = $song;
 		}
 		foreach ($users as $user) {
+			$clean_username = preg_replace("![^a-z0-9]+!i", "", $user->username);
 			$user_score = DB::select(DB::raw("SELECT jaro_winkler_similarity('" . $user->username . "','" . $term . "') AS score"));
 			$rankings[$user->username] = $user_score;
 			$username_to_suser[$user->username] = $user;
 		}
 		arsort($rankings);
-		return var_dump($rankings);
+		die(var_dump($rankings));
+		$results = array();
+		foreach ($rankings as $key => $value) {
+			# code...
+		}
 	}
 	
 
